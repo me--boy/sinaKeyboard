8c38e1950805ed6898fdc9dc46a0534eacdf5d7f
mao <maomecat@126.com>
2015-06-24 17:34:46 +0800

    Initial Commit

---
+++ sinaKeyboardTests/sinaKeyboardTests.m
@@ -0,0 +1,40 @@
+//
+//  sinaKeyboardTests.m
+//  sinaKeyboardTests
+//
+//  Created by mkq on 15/6/24.
+//  Copyright (c) 2015年 mkq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface sinaKeyboardTests : XCTestCase
+
+@end
+
+@implementation sinaKeyboardTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
